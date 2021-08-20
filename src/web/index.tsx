351f87030dcd7f487cb464aa2979446c26009bad
eight_y_88 <jadec.eight@gmail.com>
2021-08-20 14:13:19 +0900

    Create index.tsx

---
+++ src/web/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
